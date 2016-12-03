ef363f8d93057e016fd7aded2f70491f06f381cb
Balamanikandan Gunasundar <bgunasun@visteon.com>
2016-12-03 17:16:58 +0530

    Clean up directories. Remove binary files
    Signed-off-by: Balamanikandan Gunasundar <bgunasun@visteon.com>

--- test/ui-sim.c
+++ test/ui-sim.c
@@ -21,9 +21,9 @@ int main()
 	struct input_event ev[6];
 
 	fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
-	if (fd < 0) 
+	if (fd < 0)
 		error(1, errno, "Open err");
-	
+
 	ret = ioctl(fd, UI_SET_EVBIT, EV_ABS);
 	if (ret < 0)
 		goto ioctl_err;
@@ -32,7 +32,7 @@ int main()
 	if (ret < 0)
 		goto ioctl_err;
 
-	
+
 	ret = ioctl(fd, UI_SET_ABSBIT, ABS_MT_POSITION_X);
 	if (ret < 0)
 		goto ioctl_err;
@@ -41,10 +41,10 @@ int main()
 	if (ret < 0)
 		goto ioctl_err;
 
-	ret = ioctl(fd, UI_SET_ABSBIT, ABS_MT_TOUCH_MAJOR);	
+	ret = ioctl(fd, UI_SET_ABSBIT, ABS_MT_TOUCH_MAJOR);
 	if (ret < 0)
 		goto ioctl_err;
-		
+
 
 	ret = ioctl(fd, UI_SET_ABSBIT, ABS_MT_WIDTH_MAJOR);
 	if (ret < 0)
@@ -62,23 +62,23 @@ int main()
 	uidev.id.product = 0xfedc;
 	uidev.id.version = 1;
 
-	
+
 	uidev.absmin[ABS_MT_POSITION_X] = 0;
 	uidev.absmax[ABS_MT_POSITION_X] = 800;
 
-	uidev.absmin[ABS_MT_TOUCH_MAJOR] = 0; 
+	uidev.absmin[ABS_MT_TOUCH_MAJOR] = 0;
 	uidev.absmax[ABS_MT_TOUCH_MAJOR] = 255;
-	
-	uidev.absmin[ABS_MT_WIDTH_MAJOR] = 0; 
+
+	uidev.absmin[ABS_MT_WIDTH_MAJOR] = 0;
 	uidev.absmax[ABS_MT_WIDTH_MAJOR] = 255;
-	
+
 	uidev.absmin[ABS_MT_POSITION_Y] = 0;
 	uidev.absmax[ABS_MT_POSITION_Y] = 480;
-	
+
 	uidev.absmin[ABS_MT_TRACKING_ID] = 0;
 	uidev.absmax[ABS_MT_TRACKING_ID] = 3;
 
-	
+
 	ret = write(fd, &uidev, sizeof(uidev));
 	if (ret < 0)
 		goto ioctl_err;
@@ -91,7 +91,7 @@ int main()
 	/* Injecting events */
 	while (1) {
 		memset(ev, 0, sizeof(ev));
-		
+
 		ev[0].type = EV_ABS;
 		ev[0].code = ABS_MT_TRACKING_ID;
 		ev[0].value = 0;
@@ -103,34 +103,34 @@ int main()
 		ev[2].type = EV_ABS;
 		ev[2].code = ABS_MT_POSITION_X;
 		ev[2].value = 17920;
-		
+
 		ev[3].type = EV_ABS;
 		ev[3].code = ABS_MT_POSITION_Y;
 		ev[3].value = 16048;
-		
+
 		ev[4].type = EV_SYN;
 		ev[4].code = SYN_MT_REPORT;
-		
+
 		ev[5].type = EV_SYN;
 		ev[5].code= SYN_REPORT;
 
 		bufp = (uint8_t *) ev;
 		len = sizeof(ev);
-		
+
 		do {
 			wlen = write(fd, bufp, sizeof(ev));
 			if (wlen < 0)
 				error(1, errno, "Error while inj events");
 			bufp += wlen;
 			len -= wlen;
-			
+
 		} while (len != 0);
-			
+
 		sleep(1);
-	}	
-	
+	}
+
 	return 0;
-	
+
 ioctl_err:
 	puts("ioctl err");
 	return -1;
